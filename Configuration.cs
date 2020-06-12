32db8a2362ee8441c6772c60946c653e0cf58c34
Zombie <themachinehack@gmail.com>
2020-06-12 17:15:27 +0200

    First commit

---
+++ Configuration.cs
@@ -0,0 +1,15 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Darkness
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public string URL;
+        public void LoadDefaults()
+        {
+            URL = "https://plugins.darknesscommunity.club/vpn-api.php?ip=";
+        }
+    }
+}
